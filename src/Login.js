1cc6a02e87c9cea7b0e31da90f99d5f30c6bac15
Hieu Dinh <hieu.dinh2020@yahoo.com>
2020-11-03 16:06:51 -0500

    More ui changes

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,78 @@
 import React, {useState} from 'react'
 
-const Login = () => {
 
+var Login = () => {
+
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [Verifypassword, VerifyPassword] = useState('');
+    
+
     const [hasAccount, setHasAccount] = useState(false);
 
     return(
 
         <section className="login">
-            <div className="loginContainer">
-
-                <label>Email</label>
-                <input type="text" autoFocus required value ={email} placeholder ="Email"
-                onChange={(e) => setEmail(e.target.value)}/>
-
-                <label>Password</label>
-                <input type="password" required valie={password} placeholder ="Password"
-                onChange={(e) => setPassword(e.target.value)}/>
-
+        
                 <div className="btnContainer">
                     {hasAccount ? (
-                        <>
+                        <>  <div className="loginContainer">
+                        <br></br>
+                        <input type="email" autoFocus required value ={email} placeholder ="Email"
+                        onChange={(e) => setEmail(e.target.value)}/>
+
+                        <br></br>
+                        <input type="password" required valie={password} placeholder ="Password"
+                        onChange={(e) => setPassword(e.target.value)}/>
+                        <br></br>
+
                         <button>Sign in</button>
                         <p> Don't have an account? <span onClick={() => setHasAccount(!hasAccount)}>Sign up</span></p>
+                        
+                      
+                        </div>
                         </>
                     ) : (
-                        <>
-                        <button>Sign up</button>
-                        <p> Have an account? <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span></p>
+                        <>   
+                        <div className="loginContainer"> 
+
+                            <div className="registerName">
+                                <div className="firstnameBox">
+                                    <input type="fname" autoFocus required value ={fname} placeholder ="First Name" 
+                                    onChange={(e) => setFname(e.target.value)}/>
+                                </div>
+                                
+                                <div className="lastnameBox">
+                                    <input type="lname" autoFocus required value ={lname} placeholder ="Last Name" 
+                                    onChange={(e) => setLname(e.target.value)}/>
+                                </div>
+                            </div>
+
+                            <br></br>
+                            <input type="email" autoFocus required value ={email} placeholder ="Email"
+                            onChange={(e) => setEmail(e.target.value)}/>
+
+                            <br></br>
+                            <input type="password" required valie={password} placeholder ="Password"
+                            onChange={(e) => setPassword(e.target.value)}/>
+
+                            <br></br>
+                            <input type="Verifypassword" required valie={Verifypassword} placeholder ="Confirm Password"
+                            onChange={(e) => VerifyPassword(e.target.value)}/>
+                            <br></br>
+
+                            <button>Sign up</button>
+                            <p> Have an account? <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span></p>
+                     
+
+
+                        </div>
                         </>
                     )
                     }
-                </div>
-
+                
             </div>
         </section>
 
